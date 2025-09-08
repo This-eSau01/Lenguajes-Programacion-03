66d1cb5a0f133fbafab61b4554df973d8adf8ba8
Jnzh <johnmcanales16@gmail.com>
2025-09-07 23:53:14 -0500

    Update ServicioLimpieza.java

--- Exp/ServicioLimpieza.java
+++ Exp/ServicioLimpieza.java
@@ -1,7 +1,5 @@
 package hotel;
 
-// Principio de Responsabilidad Unica (SRP)
-// Solo se encarga de la logica de asignacion de personal.
 public class ServicioLimpieza implements GestionPersonal {
     @Override
     public void asignarPersonalLimpieza(PersonalLimpieza personal, Habitacion habitacion) {

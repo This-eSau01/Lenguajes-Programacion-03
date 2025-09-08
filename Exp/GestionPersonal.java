970239fed0d2fc5acf4f17f05898adb2399093c3
Jnzh <johnmcanales16@gmail.com>
2025-09-07 23:51:09 -0500

    commit

--- Exp/GestionPersonal.java
+++ Exp/GestionPersonal.java
@@ -1,7 +1,6 @@
 package hotel;
 
-// Principio de Segregacion de Interfaces (ISP)
-// Interfaz especifica para la gestion de personal.
+
 public interface GestionPersonal {
     void asignarPersonalLimpieza(PersonalLimpieza personal, Habitacion habitacion);
 }

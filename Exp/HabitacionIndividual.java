3cc4def2f9b35e7f9415d1f91a6f9c45ea9d4f20
Jnzh <johnmcanales16@gmail.com>
2025-09-07 23:51:55 -0500

    CommitHI

--- Exp/HabitacionIndividual.java
+++ Exp/HabitacionIndividual.java
@@ -1,7 +1,5 @@
 package hotel;
 
-// Principio Abierto/Cerrado (OCP) y Sustitucion de Liskov (LSP)
-// Se pueden agregar nuevos tipos de habitacion sin modificar el codigo de reserva.
 public class HabitacionIndividual implements TipoHabitacion {
     @Override
     public String getNombre() { return "Individual"; }

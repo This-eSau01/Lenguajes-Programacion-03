928ba4a4ec92789c5811b1e226d124e734fcb057
Jnzh <johnmcanales16@gmail.com>
2025-09-07 23:52:25 -0500

    COmiit

--- Exp/PoliticaCancelacion.java
+++ Exp/PoliticaCancelacion.java
@@ -1,7 +1,5 @@
 package hotel;
 
-// Principio Abierto/Cerrado (OCP)
-// Permite agregar nuevas politicas de cancelacion.
 public interface PoliticaCancelacion {
     double calcularMontoAReembolsar(double precioReserva, long diasRestantes);
 }

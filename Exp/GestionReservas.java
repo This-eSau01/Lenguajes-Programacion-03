0457a1e201baff40d35f05e160e33911244baee0
Jnzh <johnmcanales16@gmail.com>
2025-09-07 23:51:26 -0500

    CommitGD

--- Exp/GestionReservas.java
+++ Exp/GestionReservas.java
@@ -2,8 +2,6 @@ package hotel;
 
 import java.time.LocalDate;
 
-// Principio de Segregacion de Interfaces (ISP)
-// Interfaz pequena y especifica para la gestion de reservas.
 public interface GestionReservas {
     Reserva realizarReserva(Cliente cliente, String tipoHabitacion, LocalDate fechaInicio, LocalDate fechaFin);
     boolean cancelarReserva(Reserva reserva);
